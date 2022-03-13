a2a108a9a1a5ef9f9ffc897145cc5ea31224ce30
Guilherme Rocha <jguilherme.1997@gmail.com>
2022-03-13 17:16:00 -0300

    teste inicial

---
+++ programa.rb
@@ -0,0 +1,20 @@
+#comentários em ruby....
+#teste de comentários...
+
+a, b = 10, 3
+   
+valor1 = a + b
+valor2 = a - b
+valor3 = a * b
+valor4 = a / b
+valor5 = a % b
+valor6 = a ** b
+valor7 = a + (b - a)
+
+puts(valor1)
+puts(valor2)
+puts(valor3)
+puts(valor4)
+puts(valor5)
+puts(valor6)
+puts(valor7)

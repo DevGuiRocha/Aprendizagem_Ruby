000e6774dbee84e16e62f0f5677fc303b168794e
João Guilherme <jguilherme.1997@gmail.com>
2022-11-14 15:00:32 -0300

    Accessors e variáveis virtuais

--- testeMetodos.rb
+++ testeMetodos.rb
@@ -1,22 +1,24 @@
 
 class Carro
+    attr_accessor :marca, :modelo
+    #attr_reader - Método para realizar apenas leituras de variável
+    #attr_writer - Método para realizar apenas escritas em variável
+    
     def velocidade_maxima
         130
     end
 
-    def marca_carro(marca)
-        @marca = marca
-    end
-
-    def marca
-        @marca
+    def descricao
+        "Marca #{@marca} e Modelo #{@modelo}"
     end
 end
 
 novo_carro = Carro.new
 
-novo_carro.marca_carro("Ford")
-
+novo_carro.marca = "Mercedes"
+novo_carro.modelo = "Classe A"
 
 puts ("A velocidade máxima é de #{novo_carro.velocidade_maxima}")
-puts ("A marca deste carro é #{novo_carro.marca}")
+puts ("A marca deste carro é #{novo_carro.marca} e o modelo é #{novo_carro.modelo}")
+
+puts(novo_carro.descricao)

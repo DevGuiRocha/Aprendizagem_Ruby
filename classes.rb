d3ae607dad521c5c3951d5841f8e062385d5e8c2
João Guilherme <jguilherme.1997@gmail.com>
2022-11-17 16:34:09 -0300

    Visibilidade de métodos

--- classes.rb
+++ classes.rb
@@ -1,10 +1,16 @@
 class Automovel
     def self.tipo_cambio
-
+        puts("Manual")
     end
 
     def acelera
+        verifica_combustivel()
         puts("Acelera esse carro....")
+    end
+
+    private
+    def verifica_combustivel
+        puts("Verificando combustível...")
     end
 end
 

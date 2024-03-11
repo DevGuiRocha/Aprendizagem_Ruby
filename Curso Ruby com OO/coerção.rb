d1d25af81f408bdcbb2049c7016d84971706825d
João Guilherme <jguilherme.1997@gmail.com>
2024-03-11 12:55:16 -0300

    teste de conversão de variáveis

---
+++ Curso Ruby com OO/coerção.rb
@@ -0,0 +1,15 @@
+puts "Digite sua idade"
+v1 = gets.chomp
+v2 = v1 + 1.to_s
+
+=begin
+    to_i
+        converte para inteiro
+    to_f
+        converte para float
+    to_s
+        converte para string
+=end
+
+
+puts "Ano que vem, sua idade será #{v2}"

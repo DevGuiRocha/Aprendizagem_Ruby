b6e0c8964b1b9e2b5345b2e0708f99a376f59db2
João Guilherme <jguilherme.1997@gmail.com>
2024-07-17 21:15:50 -0300

    aprendendo a utilizar operadores de atribuição

---
+++ Curso Ruby com OO/operadores_atrib.rb
@@ -0,0 +1,28 @@
+a = 5
+a += 3
+b = 5
+b -+ 3
+c = 5
+c *= 3
+d = 5
+d /= 3
+e = 5
+e %= 3
+f = 5
+f **= 3
+
+=begin
++= -> adiciona um valor a variável
+-= -> subtrai um valor a variável
+*= -> multiplica um valor a variável
+/= -> divide um valor a variável
+%= -> adiciona o módulo a variável
+**= -> eleva a variável a um valor 
+=end
+
+puts a
+puts b
+puts c
+puts d
+puts e
+puts f
